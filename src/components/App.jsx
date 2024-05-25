009afce0dfe5b77863d5fad5ee071b64b889f736
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-05-25 23:30:49 +0530

    Created Header & footer

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header  from "./Header";
+import "../styles.css";
+import Footer from "./Footer";
+import Note  from "./Note";
+
+function App() {
+  return (
+    <div>
+   <Header/>
+   <Note />
+    <Footer/>
+    </div>
+  );
+}
+
+export default App;
